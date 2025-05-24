236aa5e90a5622cc3cd653b951e56055a0d3ae8f
Ravi kumar <159228634+Ravi12345677@users.noreply.github.com>
2025-05-24 22:49:50 +0530

    this voice chat used only python

---
+++ first.py
@@ -0,0 +1,15 @@
+import os
+
+import eel
+
+from Engine.features import *
+from Engine.command import *
+
+eel.init("WWW")
+
+playAssistantSound()
+
+
+os.system('start msedge.exe --app="http://localhost:8000/index.html"')
+
+eel.start('index.html', mode=None, host='localhost', block=True)
